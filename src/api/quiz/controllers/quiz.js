0f8044101046a60d5667654a8a47be180112a024
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-04-28 13:57:50 -0700

    Modify quiz controller

--- src/api/quiz/controllers/quiz.js
+++ src/api/quiz/controllers/quiz.js
@@ -8,7 +8,7 @@ const { createCoreController } = require('@strapi/strapi').factories
 
 module.exports = createCoreController('api::quiz.quiz', ({ strapi }) =>  ({
   async findOne(ctx) {
-    const { slug } = ctx.params
+    const { id: slug } = ctx.params
     const { query } = ctx
     const entity = await strapi.service('api::quiz.quiz').findOne(slug, query)
     const sanitizedEntity = await this.sanitizeOutput(entity, ctx)
