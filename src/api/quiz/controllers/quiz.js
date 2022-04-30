28a7fa89b6072373b07f3ebbe7c6cab6e05c8d99
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-04-30 16:22:49 -0700

    Revert populate components media

--- src/api/quiz/controllers/quiz.js
+++ src/api/quiz/controllers/quiz.js
@@ -13,8 +13,9 @@ module.exports = createCoreController('api::quiz.quiz', ({ strapi }) =>  ({
     if (!query.filters) query.filters = {}
     query.filters.slug = { '$eq': slug }
 
-    // const entity = await strapi.service('api::quiz.quiz').find(query)
+    const entity = await strapi.service('api::quiz.quiz').find(query)
 
+    /*
     const entity = await strapi.service('api::quiz.quiz').find(query, {
         populate: {
             entry: {
@@ -24,6 +25,7 @@ module.exports = createCoreController('api::quiz.quiz', ({ strapi }) =>  ({
             }
         }
     })
+    */
 
     const { results } = await this.sanitizeOutput(entity, ctx)
     return this.transformResponse(results[0])
