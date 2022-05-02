b820fbda5b7bf65b75cb4eb5f1471a8b5ff8d13d
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-05-02 00:51:25 -0700

    Revert viewCount to quiz

--- src/api/quiz/routes/quiz.js
+++ src/api/quiz/routes/quiz.js
@@ -6,29 +6,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-// module.exports = createCoreRouter('api::quiz.quiz');
-
-const defaultRouter = createCoreRouter('api::quiz.quiz')
-
-const customRouter = (innerRouter, extraRoutes=[]) => {
-    let routes
-    return {
-        get prefix() {
-            return innerRouter.prefix
-        },
-        get routes() {
-            if (!routes) routes = innerRouter.routes.concat(extraRoutes)
-            return routes
-        }
-    }
-} 
-
-const myExtraRoutes = [
-    {
-        'method': 'PATCH',
-        'path': '/quizzes/:slug/view',
-        'handler': 'quiz.logView'
-    }
-]
-
-module.exports = customRouter(defaultRouter, myExtraRoutes)
+module.exports = createCoreRouter('api::quiz.quiz');
