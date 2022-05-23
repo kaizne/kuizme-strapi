cf786b1f9f629231c7ab218008dc443c542c3015
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-05-23 13:22:26 -0700

    Remove min value

--- src/api/quiz/routes/quiz.js
+++ src/api/quiz/routes/quiz.js
@@ -35,7 +35,7 @@ const myExtraRoutes = [
         method: 'PATCH',
         path: '/quizzes/:slug/dislike',
         handler: 'api::quiz.quiz.decrementLike',
-    }
+    },
 ]
 
 module.exports = customRouter(defaultRouter, myExtraRoutes)
