9d424dd12c614fac70f30adaee79bd75426559df
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-05-23 18:25:21 -0700

    Add updateLibrary

---
+++ src/extensions/users-permissions/strapi-server.js
@@ -0,0 +1,32 @@
+module.exports = (plugin) => {
+    plugin.controllers.user.updateLibrary = async (ctx) => {
+
+        const user = ctx.state.user
+        if (!user) return ctx.unauthorized()
+
+        let library = user.library
+        const url = ctx.request.body.url
+
+        if (library.includes(url)) library = library.filter(elem => elem !== url)
+        else library.push(url)
+        
+        await strapi.query('plugin::users-permissions.user').update({
+          where: { id: ctx.state.user.id },
+          data: { library: library }
+        }).then((res) => {
+          ctx.response.status = 200;
+        })
+      }
+
+    plugin.routes['content-api'].routes.push({
+        method: 'PATCH',
+        path: '/users/updateLibrary',
+        handler: 'user.updateLibrary',
+        config: {
+            prefix: '',
+            policies: []
+        }
+    })
+
+    return plugin
+}
