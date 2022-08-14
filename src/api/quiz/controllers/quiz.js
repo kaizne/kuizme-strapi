78c6cc79864313451510929435c15d548d553743
Kaizne <101931534+Kaizne@users.noreply.github.com>
2022-08-14 01:37:28 -0700

    conclusion stats for type 0 and 2

--- src/api/quiz/controllers/quiz.js
+++ src/api/quiz/controllers/quiz.js
@@ -66,29 +66,57 @@ module.exports = createCoreController('api::quiz.quiz', ({ strapi }) =>  ({
     const { slug } = ctx.params
     const { query } = ctx
     const key = query.key
+    const difficulty = query.difficulty
     if (!query.filters) query.filters = {}
     query.filters.slug = { '$eq': slug }
-    const entity = await strapi.service('api::quiz.quiz').find(query)
-    const { results } = await this.sanitizeOutput(entity, ctx)
-    const response = this.transformResponse(results[0])
+    const entity = await strapi.entityService.findMany('api::quiz.quiz', {
+        fields: ['slug', 'type', 'info', 'conclusionStats'], 
+        filters: { slug: slug },
+        populate: { entry: true, section: true }
+    })
+    const response = entity[0]
     let newStats = null
-    if (!response.data.attributes.conclusionStats) {
-        if (response.data.attributes.type === 1) {
+    if (!response.conclusionStats) {
+        if (response.type === 0 || response.type === 1) {
             newStats = {}
-            newStats['default'] = []
-            const len = Object.keys(response.data.attributes.info).length
+            newStats['default'] = {}
+            const len = Object.keys(response.info).length
             for (let i = 1; i <= len; ++i)
                 newStats['default'][i] = 0
-            newStats['default'][key]++
+        } else if (response.type === 2) {
+            newStats = {}
+            if (response.entry.length > 0) {
+                newStats = {}
+                newStats['default'] = {}
+                const len = response.entry.length
+                for (let i = 1; i <= len; ++i)
+                    newStats['default'][i] = 0
+            } else {
+                newStats = {}
+                response.section.forEach(section => {
+                    newStats[section.difficulty] = {}
+                    for (let i = 1; i <= 10; ++i)
+                        newStats[section.difficulty][i] = 0
+                })
+            }
         }
     } else {
-        newStats = response.data.attributes.conclusionStats
-        newStats[key]++
+        newStats = response.conclusionStats
+        if (response.type === 0 || response.type === 1) {
+            newStats['default'][key]++
+        }
+        else if (response.type === 2) {
+            if (response.entry.length > 0) {
+                newStats['default'][key]++
+            } else {
+                newStats[difficulty][key]++
+            }
+        }
     }
     return await strapi.query('api::quiz.quiz')
         .update({
-            where: {slug: response.data.attributes.slug},
-            data: {conclusionStats: newStats}
+            where: { slug: response.slug },
+            data: { conclusionStats: newStats }
         })
   }
 }))
